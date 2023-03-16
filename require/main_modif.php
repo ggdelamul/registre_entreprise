1294ceea64bf7a030aee45077eb66c8813120d4d
jeremy <projeremylegendre@gmail.com>
2023-03-16 21:44:05 +0100

    modif main_modif

--- require/main_modif.php
+++ require/main_modif.php
@@ -2,6 +2,6 @@
 require "fonction.php";
 $connect = connexion();
 ///////////////////modif de donnee
-var_dump($_POST["nom"] . " " . $_POST["prenom"] . " " . $_POST["id"]);
+// var_dump($_POST["nom"] . " " . $_POST["prenom"] . " " . $_POST["id"]);
 modifier($connect, $_POST["nom"], $_POST["activite"], $_POST["siret"], $_POST["date"], $_POST["id"]);
 header("location:../showAlertModif.php");
